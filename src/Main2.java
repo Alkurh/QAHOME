ea4cf9434b285560dd95a40ba2a98a51dcb79309
Alkurh <alkurh@gmail.com>
2018-08-30 20:35:09 +0300

    Map

---
+++ src/Main2.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class Main2 {
+    public static void main(String[] args) {
+        HashMap<String,Integer> hash = new HashMap<>();
+        hash.put("Ales",26);
+        hash.put("Siarhei",25);
+        hash.put("Vasil",27);
+
+    }
+}
